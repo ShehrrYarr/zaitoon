db4049bdbae8a29ead02a5cf3ef1360a9e9a9f96
Shehrryarr <shehrryarr@gmail.com>
2026-01-09 03:50:16 +0500

    Sold function in all inventory

--- resources/views/allinventory.blade.php
+++ resources/views/allinventory.blade.php
@@ -1,6 +1,116 @@
 @extends('user_navbar')
 @section('content')
 
+ {{-- Edit For Sold Modal --}}
+
+    <div class="modal fade" id="exampleModal3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Sold Mobile</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="form" id="soldmobile" action="{{ route('sellMobile') }}" method="post"
+                        enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="form-body">
+
+                            <div class="mb-1">
+                                <label for="mobile_name" class="form-label">Mobile Name</label>
+                                <input class="form-control" hidden  name="id" id="sid"
+                                    value="">
+                                <input type="text" class="form-control" id="smobile_name" name="mobile_name" required
+                                    readonly>
+                            </div>
+
+                            <div class="mb-1" style="display: none">
+                                <label for="imei_number" class="form-label">IMEI Number</label>
+                                <input type="text" class="form-control" id="simei_number" name="imei_number"
+                                    required>
+                            </div>
+
+                            <div class="mb-1">
+                                <label for="availability" class="form-label">Availability</label>
+                                <select class="form-control" id="savailability" name="availability" required>
+                                    <option value="Available">Available</option>
+                                    <option value="Sold">Sold</option>
+                                    <option value="Pending">Pending</option>
+                                </select>
+                            </div>
+
+                            <div class="mb-1">
+                                <label for="customer_name" class="form-label">Customer Name</label>
+
+                                <input type="text" class="form-control" id="customer_name" name="customer_name"
+                                    required>
+                            </div>
+
+                            <div class="mb-1" style="display: none">
+                                <label for="sim_lock" class="form-label">SIM Lock</label>
+                                <select class="form-control" id="ssim_lock" name="sim_lock" required>
+                                    <option value="J.V">J.V</option>
+                                    <option value="PTA">PTA</option>
+                                    <option value="Non-PTA">Non-PTA</option>
+                                </select>
+                            </div>
+                            <div class="mb-1" style="display: none">
+                                <label for="is_approve" class="form-label">SIM Lock</label>
+                                <select class="form-control" id="sis_approve" name="is_approve">
+                                    <option value="Approved">Approved</option>
+                                    <option selected value="Not_Approved">Not_Approved</option>
+
+                                </select>
+                            </div>
+
+                            <div class="mb-1" style="display: none">
+                                <label for="color" class="form-label">Color</label>
+                                <input type="text" class="form-control" id="scolor" name="color" required>
+                            </div>
+
+                            <div class="mb-1" style="display: none">
+                                <label for="storage" class="form-label">Storage</label>
+                                <input type="text" class="form-control" id="sstorage" name="storage" required>
+                            </div>
+                            {{-- <div class="mb-1" style="display: none">
+                                <label for="battery_health" class="form-label">Battery Health</label>
+                                <input type="text" class="form-control" id="sbattery_health" name="battery_health"
+                                    required>
+                            </div> --}}
+
+                            <div class="mb-1" style="display: none">
+                                <label for="cost_price" class="form-label">Cost Price</label>
+                                <input type="number" class="form-control" id="scost_price" name="cost_price" required>
+                            </div>
+
+                            <div class="mb-1">
+                                <label for="selling_price" class="form-label">Selling Price</label>
+                                <input type="number" class="form-control" id="sselling_price" name="selling_price"
+                                    required>
+                            </div>
+                        </div>
+                        <div class="form-actions">
+                            <button type="button" class="btn btn-warning mr-1" data-dismiss="modal">
+                                <i class="feather icon-x"></i> Cancel
+                            </button>
+                            <button type="submit" class="btn btn-primary" id="soldButton">
+                                <i class="fa fa-check-square-o"></i> Save
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    {{-- End Edit For Sold Modal --}}
+
  {{-- Transfer Modal --}}
  <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2"
  aria-hidden="true">
@@ -126,6 +236,7 @@
                                         <th>Cost Price</th>
                                         <th>Selling Price</th>
                                         <th>Availability</th>
+                                        {{-- <th>Availability</th> --}}
                                         <th>Received From</th>
                                         <th>Received Date</th>
                                         <th>Transfer</th>
@@ -156,11 +267,18 @@
                                                 <td>{{ $item->description }}</td>
                                                 <td>{{ $item->cost_price }}</td>
                                                 <td>{{ $item->selling_price }}</td>
-                                                <td>
+                                                 <td>
+                                                      <a href="" onclick="sold({{ $item->id }})"
+                                                    data-toggle="modal" data-target="#exampleModal3">
+                                                    <span class="badge badge-success">{{ $item->availability }}</span>
+
+                                                </a>
+                                                </td>
+                                                {{-- <td>
                                                     <a href="#">
                                                         <span class="badge badge-primary">{{ $item->availability }}</span>
                                                     </a>
-                                                </td>
+                                                </td> --}}
                                                 <td>N/A</td>
                                                 <td>N/A</td>
                                                 <td><a href="" onclick="transfer({{ $item->id }})"
@@ -192,11 +310,18 @@
                                                 <td>{{ $item->mobile->cost_price }}</td>
                                                 <td>{{ $item->mobile->selling_price }}</td>
                                                 <td>
+                                                      <a href="" onclick="sold({{ $item->mobile->id }})"
+                                                    data-toggle="modal" data-target="#exampleModal3">
+                                                    <span class="badge badge-success">{{ $item->mobile->availability }}</span>
+
+                                                </a>
+                                                </td>
+                                                {{-- <td>
                                                     <a href="#">
                                                         <span
                                                             class="badge badge-primary">{{ $item->mobile->availability }}</span>
                                                     </a>
-                                                </td>
+                                                </td> --}}
                                                 <td>{{ $item->fromUser->name }}</td>
                                                 <td>{{ $item->created_at }}</td>
                                                 <td><a href="" onclick="transfer({{ $item->mobile->id }})"
@@ -261,6 +386,39 @@
             });
         }
         // End Transfer Function
+
+         //  Edit for Sold Function
+        function sold(value) {
+            console.log(value);
+            var id = value;
+            $.ajax({
+                type: "GET",
+                url: '/editmobile/' + id,
+                success: function(data) {
+                    
+                    $("#soldmobile").trigger("reset");
+
+                    $('#sid').val(data.result.id);
+                                  $('#smobile_name').val(data.result.mobile_name_display || '');
+
+                    $('#simei_number').val(data.result.imei_number);
+                    $('#ssim_lock').val(data.result.sim_lock);
+                    $('#scolor').val(data.result.color);
+                    $('#sstorage').val(data.result.storage);
+                    $('#sbattery_health').val(data.result.battery_health);
+                    $('#scost_price').val(data.result.cost_price);
+                    $('#savailability').val(data.result.availability);
+                    $('#sselling_price').val(data.result.selling_price);
+
+
+                },
+                error: function(error) {
+                    console.log('Error:', error);
+                }
+            });
+        }
+
+        // End Edit For Sold Function
 
         //Message Time Out
         setTimeout(function() {
